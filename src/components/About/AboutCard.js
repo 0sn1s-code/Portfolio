cd8306b7c297e2d87951f5968a7f067cec872282
Clinton Chilan <81370693+0sn1s-code@users.noreply.github.com>
2024-11-27 01:41:27 +0300

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -32,9 +32,9 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Strive to be the best developer I can be"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Chilan</footer>
         </blockquote>
       </Card.Body>
     </Card>
