8b95a788060574af0194145982470b3ec7c58be5
Clinton Chilan <81370693+0sn1s-code@users.noreply.github.com>
2024-11-27 01:24:22 +0300

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,8 +19,7 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              Ever since I was young, I have had this obsession with anything tech related, from tinkering with those analog phones to trying to understand how TVs displays pictures. It is just until I came across programming that I realised what really drove my passion. I have since dedicated my life to be the best developer I can be.
               <br />
               <br />I am fluent in classics like
               <i>
@@ -67,7 +66,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/0sn1s-code"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -77,7 +76,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://twitter.com/_chilan_"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +96,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/__chilan__"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
